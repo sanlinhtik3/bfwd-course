929dc3b7d1c7492c65b282a9aef37e35a5cd9b7c
San Lin Htike <sanlinhtike@Sans-MacBook-Pro.local>
2022-10-23 01:45:48 +0630

    dev 0.0.10 add ga

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,12 @@ import { Routes, Route } from "react-router-dom";
 import Master from "./Master";
 import {Helmet} from "react-helmet";
 
+// GA
+import ReactGA from 'react-ga';
+const TRACKING_ID = "UA-163129129-3";
+ReactGA.initialize(TRACKING_ID)
+ReactGA.pageview(window.location.pathname + window.location.search);
+
 const App = () => {
   return (
     <div className="prose prose-sm prose-slate max-w-full">
